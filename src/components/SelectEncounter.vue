66ee62337fb63d82d0f6c91398de43285ceda249
Icaro Weiler <icarog28@gmail.com>
2021-11-19 18:21:08 -0300

    init: cria primeiro componente

---
+++ src/components/SelectEncounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>TESTE</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectEncounter',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
